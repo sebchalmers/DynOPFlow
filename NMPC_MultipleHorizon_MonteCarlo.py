47eec6f6bfa0617719bac2c9e914a1f8485dddd0
sebchalmers <grosse@chalmers.se>
2013-07-25 22:06:21 +0200

    updated MonteCarlo scheme

--- NMPC_MultipleHorizon_MonteCarlo.py
+++ NMPC_MultipleHorizon_MonteCarlo.py
@@ -154,6 +154,13 @@ Load.LB['Inputs','ReactivePower'] =  -750
 Load.UB['Inputs',  'ActivePower'] = -1000
 Load.UB['Inputs','ReactivePower'] =  -750
 
+# Impose current bounds on all plants
+for plant in Net.PlantList:
+    plant.UB['Inputs','CurrentReal'] =  5.
+    plant.LB['Inputs','CurrentReal'] = -5.
+    plant.UB['Inputs','CurrentImag'] =  5.
+    plant.LB['Inputs','CurrentImag'] = -5.
+
 #################    END OF NETWORK DEFINITION    ###########################
 
 HorizonSetup = {'Min' : 6, 'Max' : 48, 'Step': 3, 'Nrealization' : 12}
